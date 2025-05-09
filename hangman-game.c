1f4ba55dbfe5bd286069ea414970d4f77091965d
Tiago <tiago.4lexandre@gmail.com>
2025-05-09 11:09:37 -0300

    Loop do-while

---
+++ hangman-game.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main()
+{
+    char secretword[20];
+    sprintf(secretword, "MUSTANG"); // Print String
+
+    int win = 0;
+    int hanged = 1;
+
+    do
+    {
+        /* code */
+    } while (!win && !hanged);
+}
